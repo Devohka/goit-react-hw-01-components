4d7c581b9a836a3e05e99f4b4be0943a53e9767a
Devohka <110058157+Devohka@users.noreply.github.com>
2024-07-16 20:05:05 +0300

    css

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -1,11 +1,24 @@
+import css from "./FriendList.module.css";
+
 const FriendList = ({ friends }) => {
+    const noOnline = {
+        backgroundColor: "red",
+        color: "red"
+    };
+    const online = {
+        backgroundColor: "green",
+        color: "green"
+    };
     return (
         friends.map(friend => {
             return (
-                <li className="item" key={friend.id}>
-                    <span className="status">{friend.isOnline ? "online" : "ofline"}</span>
-                    <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-                    <p className="name">{friend.name}</p>
+                <li className={css.item} key={friend.id}>
+                    <span >{friend.isOnline === true ?
+                        <span className={css.status} style={online}>......</span> :
+                        <span className={css.status} style={noOnline}>......</span>}
+                    </span>
+                    <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
+                    <p className={css.name}>{friend.name}</p>
                 </li>
             );
         })
